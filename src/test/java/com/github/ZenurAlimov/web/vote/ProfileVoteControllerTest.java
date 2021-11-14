c5ad591bbf3bc9f9602dc1747a4a35cb9819ced6
Zenur Alimov <zenuralimov@gmail.com>
2021-11-14 12:44:39 +0300

    add_vote_tests

---
+++ src/test/java/com/github/ZenurAlimov/web/vote/ProfileVoteControllerTest.java
@@ -0,0 +1,101 @@
+package com.github.ZenurAlimov.web.vote;
+
+import com.github.ZenurAlimov.model.Vote;
+import com.github.ZenurAlimov.repository.VoteRepository;
+import com.github.ZenurAlimov.util.VoteUtil;
+import com.github.ZenurAlimov.web.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static com.github.ZenurAlimov.util.JsonUtil.writeValue;
+import static com.github.ZenurAlimov.web.restaurant.RestaurantTestData.*;
+import static com.github.ZenurAlimov.web.user.UserTestData.*;
+import static com.github.ZenurAlimov.web.vote.VoteTestData.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ProfileVoteControllerTest extends AbstractControllerTest {
+    public static final LocalDate TODAY = LocalDate.now();
+
+    private static final String REST_URL = ProfileVoteController.REST_URL + '/';
+
+    @Autowired
+    private VoteRepository repository;
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(VOTE_TO_MATCHER.contentJson(VoteUtil.getTos(userVotes)));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getAllByRestaurantId() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "by-restaurantId" + "?restaurantId=" + KFC_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(VOTE_TO_MATCHER.contentJson(VoteUtil.getTos(List.of(vote1))));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getBetween() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "filter")
+                .param("from", TODAY.toString())
+                .param("to", TODAY.toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(VOTE_TO_MATCHER.contentJson(VoteUtil.getTos(List.of(vote2))));
+    }
+
+    @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        Vote newVote = VoteTestData.getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL + "?restaurantId=" + KING_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(newVote)));
+
+        Vote created = VoteTestData.VOTE_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newVote.setId(newId);
+        VoteTestData.VOTE_MATCHER.assertMatch(created, newVote);
+        VoteTestData.VOTE_MATCHER.assertMatch(repository.getById(newId), newVote);
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateVote() throws Exception {
+        Vote updated = VoteTestData.getUpdated();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL + "?restaurantId=" + MC_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(updated)));
+
+        if (LocalTime.now().isAfter(LocalTime.of(11, 0))) {
+            action.andExpect(status().isUnprocessableEntity());
+        } else {
+            action.andDo(print()).andExpect(status().isNoContent());
+            VoteTestData.VOTE_MATCHER.assertMatch(repository.getByDate(USER_ID, TODAY).orElse(null), VoteTestData.getUpdated());
+        }
+    }
+}
---
+++ src/test/java/com/github/ZenurAlimov/web/vote/VoteTestData.java
@@ -0,0 +1,43 @@
+package com.github.ZenurAlimov.web.vote;
+
+import com.github.ZenurAlimov.model.Vote;
+import com.github.ZenurAlimov.to.VoteTo;
+import com.github.ZenurAlimov.web.MatcherFactory;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.github.ZenurAlimov.web.restaurant.RestaurantTestData.*;
+import static com.github.ZenurAlimov.web.user.UserTestData.admin;
+import static com.github.ZenurAlimov.web.user.UserTestData.user;
+
+public class VoteTestData {
+    public static final MatcherFactory.Matcher<Vote> VOTE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Vote.class, "user", "restaurant");
+    public static MatcherFactory.Matcher<VoteTo> VOTE_TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(VoteTo.class, "");
+
+    public static final int VOTE1_ID = 1;
+    public static final int ADMIN_VOTE_ID = 3;
+
+    public static final Vote vote1 = new Vote(VOTE1_ID, LocalDate.now().minusDays(1));
+    public static final Vote vote2 = new Vote(VOTE1_ID + 1, LocalDate.now());
+    public static final Vote vote3 = new Vote(ADMIN_VOTE_ID, LocalDate.now().minusDays(1));
+
+    static {
+        vote1.setUser(user);
+        vote1.setRestaurant(kfc);
+        vote2.setUser(user);
+        vote2.setRestaurant(king);
+        vote3.setUser(admin);
+        vote3.setRestaurant(mc);
+    }
+
+    public static final List<Vote> userVotes = List.of(vote2, vote1);
+
+    public static Vote getNew() {
+        return new Vote(null, LocalDate.now());
+    }
+
+    public static Vote getUpdated() {
+        return new Vote(vote2.id(), LocalDate.now());
+    }
+}
