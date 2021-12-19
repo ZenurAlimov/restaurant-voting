3e981dcfd200cd9528c26542f59f07d8113587f0
Zenur Alimov <zenuralimov@gmail.com>
2021-12-19 04:26:10 +0300

    update_contact_info

--- src/main/java/com/github/zenuralimov/config/OpenApiConfig.java
+++ src/main/java/com/github/zenuralimov/config/OpenApiConfig.java
@@ -22,7 +22,7 @@ import org.springframework.context.annotation.Configuration;
                 title = "REST API documentation",
                 version = "1.0",
                 description = "Приложение по <a href='https://javaops.ru/view/topjava2'>курсу TopJava-2</a> (решение выпускного проекта)",
-                contact = @Contact(url = "https://github.com/ZenurAlimov", name = "Zenur Alimov", email = "zenuralimov@gmail.com")
+                contact = @Contact(url = "https://github.com/zenuralimov", name = "Zenur Alimov", email = "zenuralimov@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
